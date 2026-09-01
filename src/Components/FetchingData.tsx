23a29627bc2c21501f385951414f2e756d096aec
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-09-01 12:52:52 +0500

    useEffect() syntax and default behavior

--- src/Components/FetchingData.tsx
+++ src/Components/FetchingData.tsx
@@ -3,9 +3,14 @@ import React from "react"
 export function FetchingData(props:any){
     const [starWarsData, setStarWarsData] = React.useState(null)
 
-    fetch("https://swapi.dev/api/people/1")
-      .then(res => res.json())
-      .then(data => setStarWarsData(data))
+
+   React.useEffect(function(){
+
+       fetch("https://swapi.dev/api/people/1")
+         .then(res => res.json())
+         .then(data => setStarWarsData(data))
+         
+   }) 
 
     return(
         <div>
