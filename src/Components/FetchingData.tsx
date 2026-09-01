d92921ca153854549238a3d65f2dea5380feca6f
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-09-01 13:07:23 +0500

    useEffect() Dependencies array

--- src/Components/FetchingData.tsx
+++ src/Components/FetchingData.tsx
@@ -1,21 +1,25 @@
 import React from "react"
 
 export function FetchingData(props:any){
-    const [starWarsData, setStarWarsData] = React.useState(null)
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
 
 
    React.useEffect(function(){
 
        fetch("https://swapi.dev/api/people/1")
          .then(res => res.json())
-         .then(data => setStarWarsData(data))
-         
-   }) 
+         .then(data => setStarWarsData(data)) 
+
+   },   [count ]) 
 
     return(
         <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => +1)}>ADD</button>  
             <pre>{JSON.stringify((starWarsData), null, 2)}</pre>
         </div>
     )
     
 }
+
